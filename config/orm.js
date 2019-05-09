c94c922e520252e6f01ecd1016dc1b602891226d
Daniel Vadil <dvadz@yahoo.com>
2019-05-09 00:36:52 -0400

    addded callback to the parameters

--- config/orm.js
+++ config/orm.js
@@ -1,24 +1,24 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function(tableName) {
+    selectAll: function(tableName, cb) {
         connection.query("SELECT * FROM ??",[tableName], function(err, result){
             if(err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
-    insertOne: function(tableName, burgerName, devoured) {
+    insertOne: function(tableName, burgerName, devoured, cb) {
         connection.query("INSERT INTO ?? (burger_name, devoured) VALUES (?, ?)",
         [tableName, burgerName, devoured], function(err, result){
             if(err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
-    updateOne: function(tableName, burgerName, devoured, id) {
+    updateOne: function(tableName, burgerName, devoured, id, cb) {
         TODO:connection.query("UPDATE ?? SET burger_name = ?, devoured = ? WHERE id = ?",
         [tableName, burgerName, devoured, id], function(err, result){
             if(err) throw err;
-            console.log(result);
+            cb(result);
         });
     }
 };
