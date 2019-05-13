3d88a29c2f240374154abb67efe729b8b635bef0
Daniel Vadil <dvadz@yahoo.com>
2019-05-13 18:49:07 -0400

    added  two other required routes

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -7,11 +7,26 @@ router.get("/", function(req, res){
     burger.all(function(result){
         console.log(result);
         res.render("index", {burgers: result});
-        // TODO: use handlebars to show result on webpage
     });
 });
 
-// TODO: add
-// TODO: devour
+// add new burger
+router.post("/api/new", function(req, res){
+    burger.add(req.body.burgerName, function(result){
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+// eat the burger TODO: use proper type
+router.post("/api/eat", function(req, res){
+    console.log("/api/eat");
+    console.log(req.body);
+
+    // TODO: devour it
+    burger.devour(req.body.burger_name, req.body.id, function(result){
+        res.redirect("/");
+    });
+});
 
 module.exports = router;
