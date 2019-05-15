44b9c48cf0efa63b030d917af16e887848121e87
Daniel Vadil <dvadz@yahoo.com>
2019-05-14 23:39:13 -0400

    added the 'eat' button

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-console.log("this is the app.js");
+$(document).ready(function(){
+    console.log("Document is ready!");
+
+    $(document).on("click", ".eatfood", function(event){
+        var id = event.target.id;
+        var burger_name = event.target.getAttribute("data-burgername");
+        
+        $.post("/api/eat", {
+            id: id,
+            burger_name: burger_name
+        })
+        .then(function(){ 
+            window.location.reload()
+        });
+    });
+});
